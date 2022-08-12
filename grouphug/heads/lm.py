561a2513cb636a7908d8d734938664dae3349aa4
Sander Land <48946947+sanderland@users.noreply.github.com>
2022-08-12 16:35:34 +0200

    0.7.2 fix mlm loss (#3)
    * fix loss
    
    Co-authored-by: Sander Land <sander@chatdesk.com>

--- grouphug/heads/lm.py
+++ grouphug/heads/lm.py
@@ -99,7 +99,7 @@ class BaseLMHead(ModelHead):
         loss_fct = CrossEntropyLoss()
         masked_lm_loss = loss_fct(prediction_scores.view(-1, self.config.vocab_size), labels.view(-1))
         if torch.isnan(masked_lm_loss) and (labels == -100).all():
-            masked_lm_loss = torch.tensor(0.0, requires_grad=True)  # * prediction_scores.sum()
+            masked_lm_loss = prediction_scores.new_zeros([], requires_grad=True)  # 0.0 with matching type and device
         return masked_lm_loss
 
     def head_output_embeddings(self):
